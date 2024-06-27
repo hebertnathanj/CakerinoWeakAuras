b5759e9902800075e8f35eb201aef9d350ca9341
Nathan Hebert <hebertnathanj@gmail.com>
2024-06-27 17:43:59 -0600

    Added GrowPower, GrowCastbar functions
    Since the heights of the bars were linked to one singular property. there probably is a more elegant way to do this but overall its fine. y_offset still does not work correctly, was changed but needs to be updated.

--- OptionsCWA.lua
+++ OptionsCWA.lua
@@ -5,6 +5,8 @@ CWA = CWA or {}
 local this = aura_env
 local config = this.config
 local CLASS = this.id:gsub("Options | CakerinoWA |", "")
+local RESOURCES_GROUP = "Resources | CakerinoWA | " .. CLASS
+local MAIN_GROUP = "Main | CakerinoWA | " .. CLASS
 
 local function SetRegionSize(region, region_width, region_height)
     region:SetRegionWidth(region_width)
@@ -53,16 +55,78 @@ function CWA.GrowMain(new_positions, active_regions)
     end
 end
 
---Function determines how tracker icons grow
-function CWA.GrowTrackers(new_positions, active_regions)
-    local active_icons = #active_regions
+function CWA.GrowResource(new_positions, active_regions)
+    local active_bars = #active_regions
 
-    if active_icons <= 0 then return end
+    if active_bars <= 0 then return end
+    local region_group = WeakAuras.GetRegion(MAIN_GROUP)
+    
+    local max_icons = min(config.main.icons_max)
+    local main_width = GetMainWidth(max_icons)
+    local resource_height = config.essentials.resource.height
+    local main_spacing = config.main.spacing
 
-    local tracker_width = config.trackers.width
-    local tracker_height = config.trackers.height
-    local tracker_spacing = config.trackers.spacing
-    local main_spacing = config.essentials.spacing
+
+    local resource_width = ((main_width + main_spacing) / active_bars) - main_spacing
+    local x, y
+    local x_offset = (main_width - resource_width) / 2
+    local y_offset = (config.main.height / 2) + main_spacing + (resource_height / 2)
+
+    for i, region_data in ipairs(active_regions) do
+        x = (i - 1) * (resource_width + main_spacing) - x_offset
+        y = y_offset
+        SetRegionSize(region_data.region, resource_width, resource_height)
+        new_positions[i] = {x, y}
+    end
+end
+
+function CWA.GrowPower(new_positions, active_regions)
+    local active_bars = #active_regions
+    if active_bars <= 0 then return end
+
+    local max_icons = min(config.main.icons_max)
+    local main_width = GetMainWidth(max_icons)
+    local power_height = config.essentials.power.height
+    local main_spacing = config.main.spacing
+
+
+    local power_width = ((main_width + main_spacing) / active_bars) - main_spacing
+    local x, y
+    local x_offset = (main_width - power_width) / 2
+    local y_offset = (config.main.height / 2) + main_spacing + (power_height / 2)
+
+    for i, region_data in ipairs(active_regions) do
+        x = (i - 1) * (power_width + main_spacing) - x_offset
+        y = y_offset
+        SetRegionSize(region_data.region, power_width, power_height)
+        new_positions[i] = {x, y}
+    end
+end
+
+function CWA.GrowCastBar(new_positions, active_regions)
+    local show_cast_bar = config.essentials.cast_bar.show
+    if not show_cast_bar then return end
+
+    local active_bars = #active_regions
+    if active_bars <= 0 then return end
+
+    local max_icons = min(config.main.icons_max)
+    local main_width = GetMainWidth(max_icons)
+    local cast_bar_height = config.essentials.cast_bar.height
+    local main_spacing = config.main.spacing
+
+
+    local cast_bar_width = ((main_width + main_spacing) / active_bars) - main_spacing
+    local x, y
+    local x_offset = (main_width - cast_bar_width) / 2
+    local y_offset = (config.main.height / 2) + main_spacing + (cast_bar_height / 2)
+
+    for i, region_data in ipairs(active_regions) do
+        x = (i - 1) * (cast_bar_width + main_spacing) - x_offset
+        y = y_offset
+        SetRegionSize(region_data.region, cast_bar_width, cast_bar_height)
+        new_positions[i] = {x, y}
+    end
 end
 
 --Determines how utility icons grow
@@ -75,30 +139,17 @@ function CWA.GrowUtilities(new_positions, active_regions)
 
 end
 
---Determines how resources will grow
-function CWA.GrowResource(new_positions, active_regions)
-    local active_bars = #active_regions
+--Function determines how tracker icons grow
+function CWA.GrowTrackers(new_positions, active_regions)
+    local active_icons = #active_regions
 
-    if active_bars <= 0 then return end
+    if active_icons <= 0 then return end
 
-    local max_icons = min(config.main.icons_max)
-    local main_width = GetMainWidth(max_icons)
-    local resource_height = config.essentials.resource.height
-    local main_spacing = config.main.spacing
-
-
-    local resource_width, x, x_offset
-    local childYOffset = aura_env.region.childYOffset or 0
-
-    for i, region_data in ipairs(active_regions) do
-            resource_width = ((main_width + main_spacing) / active_bars) - main_spacing
-            x_offset = (main_width - resource_width) / 2
-            x = (i - 1) * (resource_width + main_spacing) - x_offset
-        if not this.isImporting then
-            SetRegionSize(region_data.region, resource_width, resource_height)
-            region_data.region:SetYOffset(childYOffset + main_spacing)
-        end
-
-        new_positions[i] = {x, y}
-    end
+    local tracker_width = config.trackers.width
+    local tracker_height = config.trackers.height
+    local tracker_spacing = config.trackers.spacing
+    local main_spacing = config.essentials.spacing
 end
+
+
+
